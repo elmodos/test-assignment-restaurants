4b3973dfc3678c42dc3cae95df7e9362fbfad88c
Modo Ltunzher <offnormal@gmail.com>
2020-01-19 19:22:37 +0200

    Add: BookmarkStore, UserDefaults based store implemented

---
+++ Restaurants/Shared/BookmarkStore.swift
@@ -0,0 +1,33 @@
+//
+//  BookmarkStore.swift
+//  Restaurants
+//
+//  Created by Modo Ltunzher on 19.01.2020.
+//  Copyright © 2020 Modo Ltunzher. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+public protocol BookmarkStore {
+    associatedtype Element
+    
+    var bookmarksChanged: Observable<Void> { get }
+    func setBookmarked(_ value: Bool, element: Element)
+    func isBookmarked(_ element: Element) -> Bool
+}
+
+public class AnyBookmarkStore<TElement: Encodable & Equatable> : BookmarkStore {
+    public typealias Element = TElement
+    public private(set) var bookmarksChanged: Observable<Void> = Observable.empty()
+    public func setBookmarked(_ value: Bool, element: Element) {}
+    public func isBookmarked(_ element: Element) -> Bool { return false}
+}
+
+public extension BookmarkStore {
+    
+    func toggle(for element: Element) {
+        let isBookmarked =  self.isBookmarked(element)
+        self.setBookmarked(!isBookmarked, element: element)
+    }
+}

e6731b1387381959470b1f4c73f34068da3bc933
Modo Ltunzher <offnormal@gmail.com>
2020-01-20 22:42:54 +0200

    Upd: list view model to provide sorting options observation

--- Restaurants/ViewModels/RestaurantListViewModel.swift
+++ Restaurants/ViewModels/RestaurantListViewModel.swift
@@ -14,18 +14,26 @@ class RestaurantListViewModel: ViewModel {
     
     struct Input {
         let itemAtIndexSelected: Observable<Int>
+        let sortTapped: Observable<Void>
     }
 
     struct Output {
         let list: Driver<[RestaurantItemViewModel]>
         let showRestaurantDetails: Observable<RestaurantItemViewModel>
+        let showSortOptions: Observable<SortViewModel>
     }
 
     struct Dependencies {
         let model: RestaurantList
         let bookmarkStore: AnyBookmarkStore<String>?
+        let initialSortValue: RestaurantSortField
     }
     private let dependencies: Dependencies
+    public var currentSortField: RestaurantSortField { self.sortField.value }
+    private lazy var sortField =
+        BehaviorRelay<RestaurantSortField>(value: self.dependencies.initialSortValue)
+    // TODO: filter value here
+    let disposeBag = DisposeBag()
     
     init(dependencies: Dependencies) {
         self.dependencies = dependencies
@@ -52,9 +60,30 @@ class RestaurantListViewModel: ViewModel {
                 list[index]
             }
 
+        let showSortOptions = input.sortTapped
+            .compactMap { [weak self] _ -> SortViewModel? in
+                return self?.createRestaurantsSortViewModel(current: self!.sortField.value)
+            }
+
         return Output(
             list: mappedList,
             showRestaurantDetails: showRestaurantDetails
+            showSortOptions: showSortOptions
+    
+    func createRestaurantsSortViewModel(current: RestaurantSortField) -> SortViewModel {
+        let allOptions = RestaurantSortField.allCases
+        let index = allOptions.firstIndex(of: current) ?? -1
+        let optionNames = allOptions.map { $0.description }
+        let dependencies = SortViewModel.Dependencies(
+            options: optionNames,
+            index: index
         )
+        let sortModel = SortViewModel(dependencies: dependencies)
+        sortModel
+            .index
+            .map { allOptions[$0] }
+            .bind(to: self.sortField)
+            .disposed(by: sortModel.disposeBag)
+        return sortModel
     }
 }

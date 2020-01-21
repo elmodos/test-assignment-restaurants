cf2eadc17a30dad5651c31682af01d545b62f75a
Modo Ltunzher <offnormal@gmail.com>
2020-01-21 23:40:36 +0200

    Test: restucturize

--- Restaurants/Models/RestaurantSortComparatorTests.swift
+++ Restaurants/Models/RestaurantSortComparatorTests.swift
@@ -9,38 +9,6 @@
 import XCTest
 @testable import Restaurants
 
-internal struct MockRestaurant: Restaurant {
-    var name: String = ""
-    var status: RestaurantStatus = .unknown
-}
-
-internal struct MockSortingValues: RestaurantSortingValues {
-    var bestMatch: Float = 0
-    var newest: Float = 0
-    var ratingAverage: Float = 0
-    var distance: Float = 0
-    var popularity: Float = 0
-    var averageProductPrice: Float = 0
-    var deliveryCosts: Float = 0
-    var minCost: Float = 0
-}
-
-internal struct MockSortableRestaurant: SortableRestaurant {
-    var sortingValues: RestaurantSortingValues = MockSortingValues()
-    var name: String = ""
-    var status: RestaurantStatus = .unknown
-}
-
-class MockUserDefaults: UserDefaults {
-    var key: String = "bookmarks"
-    override func array(forKey defaultName: String) -> [Any]? {
-        if defaultName == key {
-            return ["1", "2", "3"]
-        }
-        return super.array(forKey: defaultName)
-    }
-}
-
 class RestaurantSortComparatorTests: XCTestCase {
 
     typealias Comparator = RestaurantSortComparator
